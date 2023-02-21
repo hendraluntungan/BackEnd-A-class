fee958808f8519e56941b44d6a3971e9062e40e5
Hendra Luntungan <hendraluntungan29@gmail.com>
2023-02-21 13:17:16 +0800

    add Exercise #04 (21/03/23)

---
+++ tugas-express/members.js
@@ -0,0 +1,26 @@
+module.exports  = [
+    {   
+        id: 1, 
+        nim: 105000000000, 
+        nama: "Jerico", 
+        nomor_hp: "+6281234567890"
+    }, 
+    {   
+        id: 2, 
+        nim: 105000000001, 
+        nama: "Hendra", 
+        nomor_hp: "+6281234567891"
+    },
+    {   
+        id: 3, 
+        nim: 105000000002, 
+        nama: "Sherren", 
+        nomor_hp: "+6281234567892"
+    },
+    {   
+        id: 4, 
+        nim: 105000000003, 
+        nama: "Chattline", 
+        nomor_hp: "+6281234567893"
+    },
+]
